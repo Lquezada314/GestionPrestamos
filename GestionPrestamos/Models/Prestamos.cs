d6859e1f70c5310263117b3c6ed404b00fdb545b
Quezada <lismary.qp.2007@gmail.com>
2025-05-13 18:22:25 -0400

    Crea Model para prestamos

---
+++ GestionPrestamos/Models/Prestamos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionPrestamos.Models
+{
+    public class Prestamos
+    {
+        [Key]
+        public int PrestamosId { get; set; } 
+
+        [Required(ErrorMessage = "Campo requerido")]
+        public string Concepto { get; set; } = null!;
+    }
+}
