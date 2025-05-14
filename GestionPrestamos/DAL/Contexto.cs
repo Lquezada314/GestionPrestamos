d64fb6cef783f0a50015e7876ad188b69114a486
Quezada <lismary.qp.2007@gmail.com>
2025-05-14 15:47:24 -0400

    Crea el contexto para el elemento 'Prestamos'

---
+++ GestionPrestamos/DAL/Contexto.cs
@@ -0,0 +1,10 @@
+﻿using GestionPrestamos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionPrestamos.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    public DbSet<Prestamos> Prestamos { get; set; }
+}
