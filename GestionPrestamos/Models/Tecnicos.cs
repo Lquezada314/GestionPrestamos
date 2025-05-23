79b16944b31e57a62a24b6abb260781ee835b49b
Quezada <lismary.qp.2007@gmail.com>
2025-05-23 17:59:11 -0400

    Agrega mas campos a 'Deudores'

---
+++ GestionPrestamos/Models/Tecnicos.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GestionPrestamos.Models
+{
+    public class Tecnicos
+    {
+        [Key]
+        public int TecnicoId { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        public string NombresTecnico { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal SueldoxHora { get; set; }
+    }
+}
