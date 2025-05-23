abfe73bdf6b0e9330408567c6069df10ca9f0b2e
Quezada <lismary.qp.2007@gmail.com>
2025-05-23 18:01:55 -0400

    Revert "Agrega mas campos a 'Deudores'"
    This reverts commit 79b16944b31e57a62a24b6abb260781ee835b49b.

--- GestionPrestamos/Models/Deudores.cs
+++ GestionPrestamos/Models/Deudores.cs
@@ -1,30 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GestionPrestamos.Models
 {
-
     public class Deudores
     {
         [Key]
         public int DeudorId { get; set; }
 
         [Required(ErrorMessage = "Campo requerido")]
-        public string NombresDeudor { get; set; } = string.Empty;
-
-        public string Direccion { get; set; } = string.Empty;
-
-        public DateTime FechaIngreso { get; set; } = DateTime.Now;
-
-        public int RncDeudor { get; set; }
-
-        [Column(TypeName = "decimal(18, 2)")]
-        public decimal LimiteCredito { get; set; }
+        public string Nombres { get; set; } = string.Empty;
 
         //Campo para indicar si ya saldo el prestamo o tiene un balance pendiente, o su caso esta en proceso de creacion
         public string EstadoDeuda { get; set; } = string.Empty;
-
-
-
     }
 }
